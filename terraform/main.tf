33dd22da318a36dd31a228645674246dda2a3a7e
SangHyoun Yun <hainco@gmail.com>
2024-11-20 23:56:21 +0900

    chore(terraform): comment ec2 moudle

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,11 +4,11 @@ module "vpc" {
   tag_prefix = var.tag_prefix
 }
 
-module "ec2" {
-  source = "./modules/ec2"
-
-  tag_prefix = var.tag_prefix
-}
+# module "ec2" {
+#   source = "./modules/ec2"
+#
+#   tag_prefix = var.tag_prefix
+# }
 
 #module "eks-cluster" {
 #  source = "./modules/eks"
