30c531ab6c4c6886b9517fee4869bccb962af13c
SangHyoun Yun <hainco@gmail.com>
2025-02-16 20:34:37 +0900

    feat(terraform): add destroy command for EKS module

--- terraform-modules/eks/main.tf
+++ terraform-modules/eks/main.tf
@@ -2,4 +2,9 @@ resource "null_resource" "eks-cluster" {
   provisioner "local-exec" {
     command = "eksctl create cluster -f ${var.eks_config_path}"
   }
+
+  provisioner "local-exec" {
+    when    = "destroy"
+    command = "eksctl delete cluster -f ${var.eks_config_path}"
+  }
 }
